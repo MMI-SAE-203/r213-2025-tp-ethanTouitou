a795cf4e2245e877d2e706ac7ca86596a9b364d2
ethanTouitou <150148202+ethanTouitou@users.noreply.github.com>
2025-02-07 09:06:46 +0100

    Refactor card component and backend code

--- src/backend.mjs
+++ src/backend.mjs
@@ -1,74 +1,15 @@
-/*ethan TOUITOU mmi1 c2*/
 import PocketBase from 'pocketbase';
+
 const pb = new PocketBase('http://127.0.0.1:8090');
-
-/*(async () => {
-    const maisons = await pb.collection('maison').getFullList();
-    console.table(maisons);
-})();
-(async () => {
-    const maison = await pb.collection('maison').getOne('zfc982hawwv3006');
-    console.table(maison);
-})();
-
-(async () => {
-    const maisons1 = await pb.collection('maison').getFullList({ sort: 'created' });
-    console.table(maisons1);
-})();
-
-(async () => {
-    const maisons1 = await pb.collection('maison').getFullList({ sort: '-created' });
-    console.table(maisons1);
-})();
-(async () => {
-    const maisons = await pb.collection('maison').getFullList({ filter: 'favori = true' });
-    console.table(maisons);
-})();
-(async () => {
-    const maisons = await pb.collection('maison').getFullList({ filter: 'surface > 100' });
-    console.table(maisons);
-})();
-(async () => {
-    const maisons = await pb.collection('maison').getFullList({ filter: 'surface_maison > 100 && nb_sdb >= 2' });
-    console.table(maisons);
-})();
-(async () => {
-    const maisons = await pb.collection('maison').getFullList({ filter: 'surface_maison > 100 && nb_sdb >= 3' });
-    console.table(maisons);
-})();
-(async () => {
-    const maisons = await pb.collection('maison').getFullList({
-        filter: 'surface_maison > 100',
-        sort: 'prix'
-    });
-    console.table(maisons);
-})();
-(async () => {
-    const maisons = await pb.collection('maison').getFullList({ sort: 'nom_maison' });
-    console.table(maisons);
-})();
-(async () => {
-    const maisons = await pb.collection('maison').getFullList({ filter: 'nom_maison = "Appartement Skyline"' });
-    console.table(maisons);
-})();
-(async () => {
-    const maisons = await pb.collection('maison').getFullList({
-        filter: 'nom_maison != "Appartement Skyline"',
-        sort: 'nom_maison'
-    });
-    console.table(maisons);
-})();*/
-
-
 
 export async function getOffres() {
     try {
-        let data = await pb.collection('maison').getFullList({
+        let data = await pb.collection('Maison').getFullList({
             sort: '-created',
         });
 
         data = data.map((maison) => {
-            maison.imgUrl = pb.files.getUrl(maison, maison.image_maison); 
+            maison.imageUrl = pb.files.getURL(maison, maison.image);
             return maison;
         });
 
@@ -76,5 +17,18 @@ export async function getOffres() {
     } catch (error) {
         console.log('Une erreur est survenue en lisant la liste des maisons', error);
         return [];
+    }
+}
+
+export async function getOffre(id) {
+    try {
+        let data = await pb.collection('maison').getOne(id);
+        data.imageUrl = pb.files.getURL(data, data.image);
+        console.log("data",data);
+        
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la maison', error);
+        return null;
     }
 }
